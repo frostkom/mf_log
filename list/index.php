702bb13a7cbc21ed5855eadc4a15e3c2be9d5b0f
frostkom <martin.fors@frostkom.se>
2019-11-15 06:03:28 +0100

    Make sure that Delete All button only exists when there is something to delete

--- list/index.php
+++ list/index.php
@@ -4,24 +4,27 @@ $obj_log = new mf_log();
 $obj_log->fetch_request();
 $obj_log->save_data();
 
+$tbl_group = new mf_log_table();
+
+$tbl_group->select_data(array(
+	//'select' => "*",
+	'debug' => ($_SERVER['REMOTE_ADDR'] == ""),
+));
+
 echo "<div class='wrap'>
 	<h2>"
 		.__("Log", 'lang_log');
 
 		if(!isset($_REQUEST['post_status']) || $_REQUEST['post_status'] == 'all')
 		{
-			echo "<a href='".wp_nonce_url(admin_url("admin.php?page=mf_log/list/index.php&btnLogDeleteAll".($obj_log->post_status != '' && $obj_log->post_status != 'all' ? "&post_status=".$obj_log->post_status : '')), 'log_delete_all', '_wpnonce_log_delete_all')."' class='add-new-h2' rel='confirm'>".__("Delete All", 'lang_log')."</a>";
+			if($tbl_group->num_rows > 0)
+			{
+				echo "<a href='".wp_nonce_url(admin_url("admin.php?page=mf_log/list/index.php&btnLogDeleteAll".($obj_log->post_status != '' && $obj_log->post_status != 'all' ? "&post_status=".$obj_log->post_status : '')), 'log_delete_all', '_wpnonce_log_delete_all')."' class='add-new-h2' rel='confirm'>".__("Delete All", 'lang_log')."</a>";
+			}
 		}
 
 	echo "</h2>"
 	.get_notification();
-
-	$tbl_group = new mf_log_table();
-
-	$tbl_group->select_data(array(
-		//'select' => "*",
-		'debug' => ($_SERVER['REMOTE_ADDR'] == ""),
-	));
 
 	$tbl_group->do_display();
 
