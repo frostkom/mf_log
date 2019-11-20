7fe02eea5f3bd4cbdc81a2f65e0a31adb3f566dd
frostkom <martin.fors@frostkom.se>
2019-11-20 10:57:12 +0100

    Only display Delete All button if there are multiple messages

--- list/index.php
+++ list/index.php
@@ -15,12 +15,9 @@ echo "<div class='wrap'>
 	<h2>"
 		.__("Log", 'lang_log');
 
-		if(!isset($_REQUEST['post_status']) || $_REQUEST['post_status'] == 'all')
+		if((!isset($_REQUEST['post_status']) || $_REQUEST['post_status'] == 'all') && $tbl_group->num_rows > 1)
 		{
-			if($tbl_group->num_rows > 0)
-			{
-				echo "<a href='".wp_nonce_url(admin_url("admin.php?page=mf_log/list/index.php&btnLogDeleteAll".($obj_log->post_status != '' && $obj_log->post_status != 'all' ? "&post_status=".$obj_log->post_status : '')), 'log_delete_all', '_wpnonce_log_delete_all')."' class='add-new-h2' rel='confirm'>".__("Delete All", 'lang_log')."</a>";
-			}
+			echo "<a href='".wp_nonce_url(admin_url("admin.php?page=mf_log/list/index.php&btnLogDeleteAll".($obj_log->post_status != '' && $obj_log->post_status != 'all' ? "&post_status=".$obj_log->post_status : '')), 'log_delete_all', '_wpnonce_log_delete_all')."' class='add-new-h2' rel='confirm'>".__("Delete All", 'lang_log')."</a>";
 		}
 
 	echo "</h2>"
